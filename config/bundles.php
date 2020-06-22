ccf42374cc98b069e938a08894b54b553b159fe3
Vincent PECQUERIE <contact@vincent-p.fr>
2020-06-22 12:38:19 +0200

    feat(symfony): add profiler

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];

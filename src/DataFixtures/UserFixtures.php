a8eeba700c2d36b58a7005945ac59e9aee31c0ba
Vincent PECQUERIE <contact@vincent-p.fr>
2020-06-24 19:51:38 +0200

    chore(fixtures): init doctrine fixture bundle

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,30 @@
+<?php
+namespace AppBundle\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class UserFixtures extends Fixture
+{
+
+  private $faker;
+
+  public function __construct()
+  {
+    $this->faker = Faker\Factory::create('fr_FR');
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function load(ObjectManager $manager)
+  {
+    $user = new \App\Entity\User();
+    $user->setFirstname($this->faker->firstName);
+    $user->setLastname($this->faker->lastName);
+    $user->setEmail($this->faker->email);
+    $user->setUsername($this->faker->userName);
+    $manager->persist($user);
+  }
+}

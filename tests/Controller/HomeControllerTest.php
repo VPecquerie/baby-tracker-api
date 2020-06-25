63b68143ad2178831911fd7e5d92dcdf31cc4921
Vincent PECQUERIE <contact@vincent-p.fr>
2020-06-25 10:08:22 +0000

    feat(tests): add Simple unit test

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,14 @@
+<?php 
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+  public function testHomePage()
+  {
+      $client = static::createClient();
+      $client->request('GET', '/');
+      $this->assertEquals(200, $client->getResponse()->getStatusCode());
+  }
+}
